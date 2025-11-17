7a145b6c2cad78946da45bc55ad6306b59297de4
Rafael Gomes <gomesrafael3@gmail.com>
2025-11-17 11:08:34 -0300

    Aula05

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais02 {
+public class {
     public static void main(String[] args) {
         // idade < 15 categoria infantil
         // idade >= 15 && idade < 18 categoria juvenil
